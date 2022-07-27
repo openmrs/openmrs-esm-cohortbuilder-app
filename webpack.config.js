bde02a4554f9fc1ba47321c9a15d843f6ade7dee
Ian <52504170+ibacher@users.noreply.github.com>
2022-07-27 15:04:42 -0400

    Correct the regex

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
 const config = (module.exports = require("openmrs/default-webpack-config"));
-config.scriptRuleConfig.exclude =
-  /(node_modules[^\/@openmrs\/esm\-patient\-common\-lib])/;
-
+config.scriptRuleConfig.exclude = /(node_modules(?![/\\]@openmrs\/esm-patient-common-lib))/
 module.exports = config;
