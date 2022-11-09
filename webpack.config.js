8038c1785ebb2bc71d133a3632808bf8497dcec2
Dennis Kigen <kigen.work@gmail.com>
2022-11-09 21:37:41 +0300

    (chore) Migrate from babel to swc and fix deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
 const config = (module.exports = require("openmrs/default-webpack-config"));
-config.scriptRuleConfig.exclude = /(node_modules(?![/\\]@openmrs\/esm-patient-common-lib))/
+config.scriptRuleConfig.exclude =
+  /(node_modules(?![/\\]@openmrs\/esm-patient-common-lib))/;
 module.exports = config;
