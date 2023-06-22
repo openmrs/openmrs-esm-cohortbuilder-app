7f0be2d86639ee894963979286ce632d6d1829e1
Vineet Sharma <sharmava05@gmail.com>
2023-06-23 00:47:27 +0530

    Dexie fix for failing tests

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "@swc/jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!@openmrs)"],
+  moduleNameMapper: {
+    "\\.(s?css)$": "identity-obj-proxy",
+    "@openmrs/esm-framework": "@openmrs/esm-framework/mock",
+    "^lodash-es/(.*)$": "lodash/$1",
+    "^uuid$": "<rootDir>/node_modules/uuid/dist/index.js",
+    "^dexie$": require.resolve("dexie"),
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setup-tests.ts"],
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    url: "http://localhost/",
+  },
+};
