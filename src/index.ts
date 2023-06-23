8949be037e43a2d39687539748b59d17c02ac742
Ian <52504170+ibacher@users.noreply.github.com>
2023-06-23 03:05:43 -0400

    (BREAKING) Migrate esm-cohortbuilder to use routes.json (#47)
    * (BREAKING) Migrate `esm-cohortbuilder` to use routes.json
    
    * Update to core 5.0.0
    
    * Dexie fix for failing tests
    
    * swc-node dependency resolved
    
    * Updated openmrs dependency to 5.0.2.pre-*
    
    ---------
    
    Co-authored-by: Dennis Kigen <kigen.work@gmail.com>
    Co-authored-by: Vineet Sharma <sharmava05@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,22 @@
 import { getAsyncLifecycle } from "@openmrs/esm-framework";
 
-const importTranslation = require.context(
+const moduleName = "@openmrs/esm-cohort-builder";
+
+const options = {
+  featureName: "cohort-builder",
+  moduleName,
+};
+
+export const importTranslation = require.context(
   "../translations",
   false,
   /.json$/,
   "lazy"
 );
 
-const backendDependencies = {
-  fhir2: "^1.2.0",
-  "webservices.rest": "^2.2.0",
-  reportingrest: "^1.0.0",
-  reporting: "^1.0.0",
-};
+export const cohortBuilder = getAsyncLifecycle(
+  () => import("./cohort-builder"),
+  options
+);
 
-function setupOpenMRS() {
-  const moduleName = "@openmrs/esm-cohort-builder";
-
-  const options = {
-    featureName: "cohort-builder",
-    moduleName,
-  };
-
-  return {
-    pages: [
-      {
-        load: getAsyncLifecycle(() => import("./cohort-builder"), options),
-        route: "cohort-builder",
-      },
-    ],
-    extensions: [
-      {
-        id: "system-administration-cohort-builder-card-link",
-        slot: "system-admin-page-card-link-slot",
-        load: getAsyncLifecycle(
-          () => import("./cohort-builder-admin-link.component"),
-          options
-        ),
-        online: true,
-        offline: true,
-      },
-    ],
-  };
-}
-
-export { backendDependencies, importTranslation, setupOpenMRS };
+export function startupApp() {}
