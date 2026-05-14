d37c14c5e692d85bd87fbb4e6b642a6b75123fc6
Dennis Kigen <kigen.work@gmail.com>
2026-05-14 21:51:01 +0300

    (chore) Migrate to rspack (#649)

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,2 +1,16 @@
 declare module '*.css';
 declare module '*.scss';
+
+declare interface RequireContext {
+  keys(): string[];
+  (id: string): unknown;
+  <T>(id: string): T;
+  resolve(id: string): string;
+  id: string;
+}
+
+declare namespace NodeJS {
+  interface Require {
+    context(directory: string, useSubdirectories?: boolean, regExp?: RegExp, mode?: string): RequireContext;
+  }
+}
